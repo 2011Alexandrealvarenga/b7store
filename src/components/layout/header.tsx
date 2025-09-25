b853cfa3611e9f3d32e9382dd94301870dc2667d
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2025-09-25 11:50:49 -0300

    buttons and icons

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,42 @@
+"use client"
+import Image from "next/image"
+import imglogo from '../../../public/assets/ui/logo-black.png'
+import { HeaderIcon } from "./header-icon"
+import Link from "next/link"
+
 export function Header(){
   return(
-    <header>
+    <header className="bg-white border-b border-gray-100">
       <div className="bg-black text-white text-center p-4">
         <strong>FRETE GRÁTIS</strong> para todo o Nordeste nas compras acima de R$ 199,00.  <strong>APROVEITA!</strong>
       </div>
+      <div className="w-full max-w-6xl mx-auto p-6">
+
+        <div className="flex items-center justify-between">
+          <div className="w-32">
+            <Link href={'/'}>
+              <Image 
+                src="/assets/ui/logo-black.png"
+                alt="B7store"
+                width={120}
+                height={40}
+              />
+            </Link>
+          </div>
+          <div className="flex gap-4">
+            <Link href={'/my-orders'}>
+              <HeaderIcon src="/assets/ui/user-line.png" alt="Perfil"/>
+            </Link>
+            <Link href={'/cart'}>
+              <HeaderIcon src="/assets/ui/shopping-bag-4-line.png" alt="Carrinho"/>
+            </Link>
+            <div className="md:hidden">
+              <HeaderIcon src="/assets/ui/menu-line.png" alt="menu"/>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </header>
   )
 }
